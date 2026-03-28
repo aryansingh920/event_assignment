9d15c82538264d1c02c1ba1a496394eb6c71c79f
Aryan Singh <aryansingh920@outlook.com>
2026-03-28 23:58:22 +0530

    feat: comment out reactStrictMode in Next.js configuration for deployment strategy

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,7 @@
 // client/next.config.ts
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  // reactStrictMode: false,
   output: "export", // This is CRITICAL for the Nginx strategy
 };
 
