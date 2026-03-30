b4e96f85e8214d81f2ba42d310bc03524d6ee743
Aryan Singh <aryansingh920@outlook.com>
2026-03-30 20:22:31 +0530

    feat: streamline service build process and add test script for database interaction

--- run.sh
+++ run.sh
@@ -1,18 +1,9 @@
 #!/bin/zsh
 
-docker compose down -v
+
+# Build the service
 docker compose up --build
 
-clear && docker compose down && sudo rm -rf ./postgres_data && docker compose up --build
 
-docker exec -it postgres_db psql -U admin -d mydb
-# \dt
-# SELECT COUNT(*) FROM events;
-# SELECT * FROM events LIMIT 5;
-# \q
-
-# rebuild just 1 service
-docker compose up -d --build python-consumer
-docker compose up -d --build ws-service
-docker compose up -d --build node-api
-docker compose up -d --build client
+# Get Users
+ curl -s http://localhost:8080/api/allUsers | python -m json.tool   
---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+
+docker compose down -v
+docker compose up --build
+
+clear && docker compose down && sudo rm -rf ./postgres_data && docker compose up --build
+
+docker exec -it postgres_db psql -U admin -d mydb
+# \dt
+# SELECT COUNT(*) FROM events;
+# SELECT * FROM events LIMIT 5;
+# \q
+
+# rebuild just 1 service
+docker compose up -d --build python-consumer
+docker compose up -d --build ws-service
+docker compose up -d --build node-api
+docker compose up -d --build client
